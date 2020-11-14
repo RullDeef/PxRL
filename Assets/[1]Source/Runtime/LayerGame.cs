bc6fd08954a4f2d92cafb58321719cbbe93094b2
Rull Deef <deeroll666@gmail.com>
2020-11-14 23:14:57 +0000

    add actors framework

---
+++ Assets/[1]Source/Runtime/LayerGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixeye.Actors;
+
+public class LayerGame : Layer<LayerGame>
+{
+    protected override void Setup()
+    {
+        Add<ProcessorUpdate>();
+        Add<ProcessorMazeGenerator>();
+
+        // Get<ProcessorMazeGenerator>().Generate();
+        Debug.Log("setuped");
+    }
+}
---
+++ Assets/[1]Source/Runtime/ProcessorMazeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixeye.Actors;
+
+public class ProcessorMazeGenerator : Processor
+{
+    public ProcessorMazeGenerator()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        debug.log("Generated maze");
+    }
+}
