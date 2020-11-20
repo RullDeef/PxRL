7b91f2c6d523d960762ecf356abbf1c36c450b91
Rull Deef <deeroll666@gmail.com>
2020-11-20 18:10:28 +0000

    add simple generator

---
+++ Assets/[1]Source/Editor/Maze/MazeGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MazeGenerator))]
+public class MazeGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate maze"))
+        {
+            (target as MazeGenerator).Generate();
+        }
+    }
+}
---
+++ Assets/[1]Source/Runtime/Maze/GeneratorParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[Serializable]
+public struct GeneratorTiles
+{
+    public Tile wall;
+}
+
+[CreateAssetMenu(fileName = "Generator Params", menuName = "Maze/Generator params")]
+[Serializable]
+public class GeneratorParams : ScriptableObject
+{
+    public Vector2Int dimensions;
+
+    public int roomsAmount;
+    public Vector2Int roomSize;
+
+    public GeneratorTiles tiles;
+}
